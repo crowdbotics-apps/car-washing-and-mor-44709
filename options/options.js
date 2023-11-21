f8f5a787207ef60216bfe362f389834a23f2c387
Crowdbotics <team@crowdbotics.com>
2023-11-21 19:40:40 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "car-washing-and-mor-44709",
+  url: "https://car-washing-and-mor-44709.botics.co",
+  api: "https://car-washing-and-mor-44709.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
